3af72b20b4288f94d9840dac217179ad5e7935e6
Vladimir Rudomanenko <rudomanenkovladimir@gmail.com>
2023-07-17 01:54:54 +0300

    use named parameters

--- add_stat.php
+++ add_stat.php
@@ -14,18 +14,19 @@ try {
         $config['port']
     );
 
-    $avg = $_POST['avg'];
-    $min_value = $_POST['min_value'];
-    $max_value = $_POST['max_value'];
-    $end_time = $_POST['end_time'];
-    $start_time = $_POST['start_time'];
-    $time_spent = $_POST['time_spent'];
-    $mode = $_POST['mode'];
-    $mode_count = $_POST['mode_count'];
-    $standard_deviation = $_POST['standard_deviation'];
-    $lost_quotes = $_POST['lost_quotes'];
-    $quotes_count = $_POST['quotes_count'];
-    // todo: use named parameters
+    $parameters = [
+        ':avg'                  => $_POST['avg'],
+        ':min_value'            => $_POST['min_value'],
+        ':max_value'            => $_POST['max_value'],
+        ':start_time'           => $_POST['start_time'],
+        ':end_time'             => $_POST['end_time'],
+        ':time_spent'           => $_POST['time_spent'],
+        ':mode'                 => $_POST['mode'],
+        ':mode_count'           => $_POST['mode_count'],
+        ':standard_deviation'   => $_POST['standard_deviation'],
+        ':lost_quotes'          => $_POST['lost_quotes'],
+        ':quotes_count'         => $_POST['quotes_count']
+    ];
 
     $sql = "INSERT INTO statistics (
                    avg,
@@ -39,21 +40,21 @@ try {
                    standard_deviation,
                    lost_quotes,
                    quotes_count
-                   ) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+                   ) VALUES (
+                             :avg,
+                             :min_value,
+                             :max_value,
+                             :start_time,
+                             :end_time,
+                             :time_spent,
+                             :mode,
+                             :mode_count,
+                             :standard_deviation,
+                             :lost_quotes,
+                             :quotes_count
+                             )";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([
-        $avg,
-        $min_value,
-        $max_value,
-        $start_time,
-        $end_time,
-        $time_spent,
-        $mode,
-        $mode_count,
-        $standard_deviation,
-        $lost_quotes,
-        $quotes_count
-    ]);
+    $stmt->execute($parameters);
 
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
